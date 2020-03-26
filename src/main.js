3c9e7cc8a8a271f962b9e610f4ac8e4065edfa69
Jonas Scheiwiller <git@jonasscheiwiller.ch>
2020-03-26 16:04:20 +0100

    [Update] Add vue router

--- src/main.js
+++ src/main.js
@@ -1,10 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+
 import store from './store'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  mode: 'history',
+  base: __dirname,
+  routes: [
+    { path: '/', name: 'home', component: App },
+  ]
+})
 
 new Vue({
+  router,
   el: '#app',
   store,
   render: h => h(App)
