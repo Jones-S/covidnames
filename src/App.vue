11da180e03da7ebbe449fe1df0bd1b39d3a45daf
Jonas Scheiwiller <git@jonasscheiwiller.ch>
2020-04-01 23:06:05 +0200

    [Update] Typo in meta desc

--- src/App.vue
+++ src/App.vue
@@ -161,7 +161,7 @@ export default {
       },
       meta: [
         { charset: 'utf-8' },
-        { name: 'description', content: 'Spiele CodeNames trotz Social Distancing. CovidNames ist die digital spielbare Varianten des Brettspiels CodeNames mit verbesserter Spielleiter-Ansicht und unendlichen Spiel-Möglichkeiten.' },
+        { name: 'description', content: 'Spiele CodeNames trotz Social Distancing. CovidNames ist die digital spielbare Variante des Brettspiels «CodeNames» mit verbesserter Spielleiter-Ansicht und unendlichen Spiel-Möglichkeiten.' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
         { property: 'og:image', content: ogImageUrl },
         { name: 'og:image:width', content: 1080 },
